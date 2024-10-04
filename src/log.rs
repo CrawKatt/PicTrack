312cdc80de37c52d59d0f99dee3eef4e62932160
crawkatt <man849916@gmail.com>
2024-10-03 22:22:57 -0300

    ✨ feat: add log command

--- src/log.rs
+++ src/log.rs
@@ -1,35 +1,37 @@
 use std::fs;
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, BufReader};
 use std::path::Path;
+use crate::current_branch::current_branch;
+use crate::branch::BRANCHES_DIR;
 
-// Función para leer y mostrar el historial de commits
+// Mostrar el historial de commits de la rama actual
 pub fn log_commits() -> io::Result<()> {
-    let commits_dir = Path::new(".images/commits");
+    let branch = current_branch()?;
+    let branch_dir = Path::new(BRANCHES_DIR).join(&branch);
 
-    // Verificar si el directorio de commits existe
-    if !commits_dir.exists() {
-        eprintln!("No hay commits en el repositorio.");
+    if !branch_dir.exists() {
+        eprintln!("La rama '{}' no tiene commits.", branch);
         return Ok(());
     }
 
-    // Obtener la lista de archivos de commits
-    let mut entries: Vec<_> = fs::read_dir(commits_dir)?
+    // Obtener la lista de archivos de commits en la rama actual
+    let mut entries: Vec<_> = fs::read_dir(&branch_dir)?
         .filter_map(|entry| entry.ok())
         .collect();
 
-    // Ordenar los archivos por nombre (el hash se usa como nombre)
+    // Ordenar los commits por nombre (el hash se usa como nombre)
     entries.sort_by_key(|entry| entry.file_name());
 
-    // Leer y mostrar cada archivo de commit
+    // Leer y mostrar cada commit
     for entry in entries {
         let path = entry.path();
         if path.is_file() {
             let file = fs::File::open(&path)?;
-            let reader = io::BufReader::new(file);
+            let reader = BufReader::new(file);
 
             println!("\nCommit en {}", path.display());
 
-            // Leer el archivo línea por línea y mostrar su contenido
+            // Leer el archivo línea por línea
             for line in reader.lines() {
                 let line = line?;
                 println!("{}", line);
