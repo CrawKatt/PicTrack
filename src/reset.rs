952240c0811ccaff32605db971f7693445fec90d
crawkatt <man849916@gmail.com>
2024-10-03 22:22:36 -0300

    ✨ feat: add reset command

---
+++ src/reset.rs
@@ -0,0 +1,20 @@
+use std::fs;
+use std::io;
+use std::path::Path;
+use crate::branch::BRANCHES_DIR;
+use crate::current_branch::current_branch;
+
+pub fn reset_commit(commit_hash: &str) -> io::Result<()> {
+    let branch = current_branch()?;
+    let branch_dir = Path::new(BRANCHES_DIR).join(&branch);
+    let commit_path = branch_dir.join(format!("{commit_hash}.txt"));
+
+    if commit_path.exists() {
+        fs::remove_file(commit_path)?;
+        println!("Commit {commit_hash} eliminado");
+    } else {
+        eprintln!("El commit {commit_hash} no existe en la rama actual.");
+    }
+
+    Ok(())
+}
